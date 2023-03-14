eb14c0cc77fc8c8714ea43f1c182c0c7d0b8604f
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-03-14 23:04:15 +0100

    recorrection

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ const lancer = document.querySelector('.lancer');
 const page = document.querySelector('.page');
 const coherence = document.querySelector('.coherence');
 const close = document.querySelector('.close');
+const cercle  = document.getElementById('rond')
 
 
 // affichage cohérence
 
 lancer.addEventListener('click', () => {
     accueil.style.display = 'none';
-    page.style.display = 'flex';
+    page.style.display = 'grid';
 });
+
+
 
 close.addEventListener('click', () => {
     accueil.style.display = '';
     page.style.display = 'none';
 })
 
+cercle.addEventListener('click' , () => {
+    cercle.classList.toggle('rythme37')
+})
+// cercle.addEventListener('click' , () => {
+//     cercle.classList.remove('rythme37')
+// })
 
-//T empo 
+// Tempo 
 
 
